ec0101f3d82f7fe2bc5b374ea3c4b139addf8271
Rezekiel <rezekiel@hawk.iit.edu>
2019-04-18 15:01:33 -0500

    java

--- java/site.js
+++ java/site.js
@@ -1,19 +1,7 @@
-jQuery(function ($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-});
-document.getElementById("submit").addEventListener("click", confirm);
-
-function confirm(event) {
-  for (var i = 0; i < document.getElementsByTagName("input").length; i++) {
-    if (document.getElementsByTagName("input").item(i).value === "" || (document.getElementsByTagName("input").item(i).type === "radio") && (document.getElementsByTagName("input").item(i).checked === false)) {
-      return false;
-    } else {
-      fname = document.getElementById("fname").value;
-      lname = document.getElementById("lname").value;
-      name = document.getElementById("fname").value;
-      alert("Thank you" + fname + " " + lname + " You have been added to the Exclusive Mailing List.");
-      return true;
-    }
-  }
-}
+var button=document.getElementById("entrybutton")
+button.addEventListener("click", func);
+		function func(){
+			var input=document.getElementById("entryinput").value;
+			alert("Raymond Ezekiel: "+ input);
+			document.getElementById("textoutput").innerHTML=input;
+		}
