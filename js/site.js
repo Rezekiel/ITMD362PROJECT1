b63c196a62bd07267464dd822f2740caec6e0da3
Rezekiel <rezekiel@hawk.iit.edu>
2019-04-18 21:19:45 -0500

    add onclick for the message to show

--- js/site.js
+++ js/site.js
@@ -1,7 +1,20 @@
-var button=document.getElementById("entrybutton")
-button.addEventListener("click", func);
-		function func(){
-			var input=document.getElementById("entryinput").value;
-			alert("Raymond Ezekiel: "+ input);
-			document.getElementById("textoutput").innerHTML=input;
-		}
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function ($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+document.getElementById("submit").addEventListener("click", confirm);
+
+function confirm(event) {
+  for (var i = 0; i < document.getElementsByTagName("input").length; i++) {
+    if (document.getElementsByTagName("input").item(i).value === "" || (document.getElementsByTagName("input").item(i).type === "radio") && (document.getElementsByTagName("input").item(i).checked === false)) {
+      return false;
+    } else {
+      fname = document.getElementById("fname").value;
+      lname = document.getElementById("lname").value;
+      name = document.getElementById("fname").value;
+      alert("Thank you " + fname + " " + lname + " for subscribing.");
+      return true;
+    }
+  }
+}
